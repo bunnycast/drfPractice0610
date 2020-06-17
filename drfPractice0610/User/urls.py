70fedabcda0fc9314de8904ff3f46d4627186310
Daniel Kim <berzzubunny@gmail.com>
2020-06-17 22:43:16 +0900

    feat

--- drfPractice0610/User/urls.py
+++ drfPractice0610/User/urls.py
@@ -1,7 +1,14 @@
+from django.conf.urls import url
+from rest_framework.authtoken import views
 from rest_framework.routers import SimpleRouter
 from User.views import UserViewSet
 
 router = SimpleRouter()
 router.register(r'api/users', UserViewSet, basename='users')
 
-urlpatterns = router.urls
+urlpatterns = [
+    url(r'api-token-auth/', views.obtain_auth_token),
+]
+
+urlpatterns += router.urls
+
